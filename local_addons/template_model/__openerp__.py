a881b586cc1e195e40a27d15964efe653f044ffd
Mohamed Omar <mohamed.omar@core-bpo.com>
2016-07-03 13:00:28 +0200

    init of AKRED Project

---
+++ local_addons/template_model/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Title',
+    'version': '1.1',
+    'summary': 'Short subtitle phrase',
+    'description': """Long description""",
+    'category': 'Uncategorized',
+    'website': 'http://www.example.com',
+    'images': [],
+    'depends': ['base', 'decimal_precision'],
+    'data': ['views/library_book.xml'],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+}
