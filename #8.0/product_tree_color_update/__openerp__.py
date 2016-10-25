b690f134f0db678b2f59abb7b626013ee5ca6992
Moahemed <mohamed.sharaf@core-bpo.com>
2016-10-25 17:01:44 +0200

    after add descreption

--- #8.0/product_tree_color_update/__openerp__.py
+++ #8.0/product_tree_color_update/__openerp__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'product',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','stock','product'],
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'sale_inherit_workflow.xml',
-        'templates.xml',
+        'views/templates.xml',
     ],
     # only loaded in demonstration mode
 
