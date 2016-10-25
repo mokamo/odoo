8ff860183e0dfaa2171d4e3959a533b7351f8bec
Moahemed <mohamed.sharaf@core-bpo.com>
2016-10-25 14:30:24 +0200

    local_addons/

--- local_addons/template_model/models/library_book.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-from openerp import models,fields
-
-class ProductProduct(models.Model):
-    _inherit = 'product.template'
-    least_quantity = fields.Integer(string="Least Quantity", required=False, )
-
