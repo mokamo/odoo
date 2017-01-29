26b44fa038668943d58c4bf153f0f63bcfee1d6e
Moahemed <mohamed.sharaf@core-bpo.com>
2017-01-29 18:30:31 +0200

    latest

--- product_tree_color_update/__openerp__.py
+++ product_tree_color_update/__openerp__.py
@@ -18,7 +18,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'product',
+    'category': 'warehouse',
     'version': '8.0.1.0.0',
 
     # any module necessary for this one to work correctly
